3c26fd46d7ebaab3f5962204550ea4f4685d4409
weipeilun <weipeilun0217@gmail.com>
2025-01-11 11:58:59 +0800

    modify luchentec configs

--- start_train_luchentec.sh
+++ start_train_luchentec.sh
@@ -1,4 +1,7 @@
-cd /root/my_env/LLaMA-Factory
-PYTHONPATH=/root/my_env/LLaMA-Factory:$PYTHONPATH
-PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-/data/anaconda3/envs/llama-factory/bin/python /root/my_env/LLaMA-Factory/src/llamafactory/cli.py train examples/train_lora/qwen2_vl_luchentec.yaml > train.log 2>&1
+base_dir=/root/filesystem/workspace/LLaMA-Factory
+cd $base_dir
+export PYTHONPATH=$base_dir:$base_dir/src:$base_dir/data:$PYTHONPATH
+export NCCL_P2P_DISABLE="1"
+export NCCL_IB_DISABLE="1"
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+python $base_dir/cli.py train examples/train_lora/qwen2_vl_luchentec.yaml
