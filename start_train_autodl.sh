19dcb041a9d8c716447c8e0f3fcb9ddea2bb46e6
weipeilun <weipeilun0217@gmail.com>
2025-01-17 21:53:53 +0800

    add autodl config

---
+++ start_train_autodl.sh
@@ -0,0 +1,7 @@
+base_dir=/root/workspace/LLaMA-Factory
+cd $base_dir
+export PYTHONPATH=$base_dir:$base_dir/src:$base_dir/data:$PYTHONPATH
+export NCCL_P2P_DISABLE="1"
+export NCCL_IB_DISABLE="1"
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+python $base_dir/src/llamafactory/cli.py train examples/train_lora/qwen2_vl_autodl.yaml
