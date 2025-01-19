448b647b9c9bcfa646845e3099bb7d16308fdb52
weipeilun <weipeilun0217@gmail.com>
2025-01-19 21:59:16 +0800

    update training parameters

--- start_train.sh
+++ start_train.sh
@@ -1,4 +1,7 @@
-cd /home/weipeilun/playground/racing/LLaMA-Factory
-PYTHONPATH=/home/weipeilun/playground/racing/LLaMA-Factory:$PYTHONPATH
-PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-/data/anaconda3/envs/llama-factory/bin/python /home/weipeilun/playground/racing/LLaMA-Factory/src/llamafactory/cli.py train examples/train_lora/qwen2_vl.yaml > train.log 2>&1
+base_dir=/home/weipeilun/playground/racing/LLaMA-Factory
+cd $base_dir
+export PYTHONPATH=$base_dir:$base_dir/src:$base_dir/data:$PYTHONPATH
+export NCCL_P2P_DISABLE="1"
+export NCCL_IB_DISABLE="1"
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+python $base_dir/src/llamafactory/cli.py train examples/train_lora/qwen2_vl.yaml
